72937cb3f9da0b8d89cf411008f90407c0d80de9
shenzhoushihao <1665345498@qq.com>
2019-03-22 10:57:50 +0800

    add 注释

--- boot-dbb-consumer/src/main/java/com/great/dubbo/consumer/config/DubboConfig.java
+++ boot-dbb-consumer/src/main/java/com/great/dubbo/consumer/config/DubboConfig.java
@@ -2,7 +2,12 @@ package com.great.dubbo.consumer.config;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 
+ * @author roc
+ *
+ */
 @Configuration
 public class DubboConfig {
-	
+
 }
